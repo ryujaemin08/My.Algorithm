728393182c9b067c12e36024ae90ee628c0ef6e3
Ryujaemin <162968107+ryujaemin08@users.noreply.github.com>
2026-02-23 22:05:11 +0900

    [Bronze V] Title: 알고리즘 수업 - 알고리즘의 수행 시간 1, Time: 104 ms, Memory: 14448 KB -BaekjoonHub

---
+++ 백준/Bronze/24262. 알고리즘 수업 － 알고리즘의 수행 시간 1/알고리즘 수업 － 알고리즘의 수행 시간 1.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+
+public class Main
+{
+    
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+		
+		bw.write("1\n0");
+	    
+		bw.close();
+		br.close();
+	}
+}
